5ee45bfe81363a8b710cacf36a54c1f272290874
jackboxx <lucygschwantner@gmail.com>
2023-04-01 21:27:42 +0200

    "solve" problem 50

--- problem-50/src/main.rs
+++ problem-50/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", 10.0_f64.powi(5));
+    println!("{}", 10.0_f64.powi(-5));
+    println!("{}", 0.44528_f64.powi(0));
+    println!("{}", 0.00001_f64.powi(2147483647));
+    println!("{}", 2.0_f64.powi(-2147483647));
+}
+
+fn my_pow(x: f64, n: i32) -> f64 {
+    x.powi(n)
 }
