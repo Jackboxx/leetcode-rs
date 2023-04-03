07b69d291599858f6e1e73069dff157d1ba071d5
Jackboxx <gschwantnermoritz@gmail.com>
2023-04-03 19:49:08 +0200

    "solve" problem 80

--- problem-80/src/main.rs
+++ problem-80/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let mut vec = vec![1, 1, 1, 2, 2, 3];
+    println!("{}", remove_duplicates(&mut vec));
+    println!("{:?}", vec);
+}
+
+fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    let mut count = 0;
+    let mut index = 1;
+
+    for i in 1..nums.len() {
+        if nums[i] == nums[i - 1] {
+            count += 1;
+        } else {
+            count = 0;
+        }
+
+        if count <= 1 {
+            nums[index] = nums[i];
+            index += 1;
+        }
+    }
+
+    index as i32
 }
