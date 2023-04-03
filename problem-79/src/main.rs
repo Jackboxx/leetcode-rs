c29d1e9e338d4eb3b0bfe65e6c2048ca1a99cbb3
Jackboxx <gschwantnermoritz@gmail.com>
2023-04-03 19:35:00 +0200

    solve problem 79

--- problem-79/src/main.rs
+++ problem-79/src/main.rs
@@ -9,7 +9,6 @@ fn main() {
     );
 
     println!("{res}");
-    return;
 
     let res = exist(
         vec![
@@ -30,11 +29,9 @@ fn exist(board: Vec<Vec<char>>, word: String) -> bool {
         for x in 0..board.get(0).unwrap().len() {
             if board[y][x] == first_letter {
                 let mut visited = vec![vec![false; board.get(0).unwrap().len()]; board.len()];
-                println!("start: {x} {y}");
                 if find_next(&board, &mut visited, (x, y), word.clone(), 1) {
                     return true;
                 }
-                println!("{visited:?}");
             }
         }
     }
@@ -49,7 +46,6 @@ fn find_next(
     word: String,
     current_char: usize,
 ) -> bool {
-    println!("current: {start_pos:?}");
     if word.len() <= current_char {
         return true;
     }
@@ -64,14 +60,20 @@ fn find_next(
         .unwrap_or(&vec![])
         .get(x.checked_sub(1).unwrap_or(usize::MAX))
     {
-        if *left == target && !visited[y][x - 1] {
-            return find_next(board, visited, (x - 1, y), word, current_char + 1);
+        if *left == target
+            && !visited[y][x - 1]
+            && find_next(board, visited, (x - 1, y), word.clone(), current_char + 1)
+        {
+            return true;
         }
     }
 
     if let Some(right) = board.get(y).unwrap_or(&vec![]).get(x + 1) {
-        if *right == target && !visited[y][x + 1] {
-            return find_next(board, visited, (x + 1, y), word, current_char + 1);
+        if *right == target
+            && !visited[y][x + 1]
+            && find_next(board, visited, (x + 1, y), word.clone(), current_char + 1)
+        {
+            return true;
         }
     }
 
@@ -80,17 +82,20 @@ fn find_next(
         .unwrap_or(&vec![])
         .get(x)
     {
-        println!("top: {top}");
-        if *top == target && !visited[y - 1][x] {
-            return find_next(board, visited, (x, y - 1), word, current_char + 1);
+        if *top == target
+            && !visited[y - 1][x]
+            && find_next(board, visited, (x, y - 1), word.clone(), current_char + 1)
+        {
+            return true;
         }
     }
 
-    println!("what");
     if let Some(bottom) = board.get(y + 1).unwrap_or(&vec![]).get(x) {
-        println!("bottom: {bottom}");
-        if *bottom == target && !visited[y + 1][x] {
-            return find_next(board, visited, (x, y + 1), word, current_char + 1);
+        if *bottom == target
+            && !visited[y + 1][x]
+            && find_next(board, visited, (x, y + 1), word, current_char + 1)
+        {
+            return true;
         }
     }
 
